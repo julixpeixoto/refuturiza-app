0751f86c20c08470622d3e49dede9f25e88e856f
Julierme Peixoto <julierme2005@hotmail.com>
2022-04-02 14:01:57 -0300

    Added routes protection with auth

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,14 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/api', [PanelController::class, 'show']);
-
 Auth::routes();
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/api', [PanelController::class, 'show']);
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-Route::post('/profile/update', [ProfileController::class, 'update'])->name('profileUpdate');
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+    Route::post('/profile/update', [ProfileController::class, 'update'])->name('profileUpdate');
 
-Route::get('/users', [UsersController::class, 'index'])->name('users');
+    Route::get('/users', [UsersController::class, 'index'])->name('users');
+});
