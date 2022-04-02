feadc40f47542f8393b8b3c456b08575917390d0
Julierme Peixoto <julierme2005@hotmail.com>
2022-04-02 16:48:20 -0300

    Fixed menus

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,10 @@
+@php
+try {
+        $path = explode('/',url()->current())[3];
+    } catch (\Exception $e) {
+        $path = '';
+    }
+@endphp
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -27,11 +34,11 @@
                 <a href="{{ route('home') }}" class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Admin</a>
             </div>
             <nav class="text-white text-base font-semibold pt-3">
-                <a href="{{ route('home') }}" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
+                <a href="{{ route('home') }}" class="flex items-center text-white py-4 pl-6 nav-item @if($path == '') active-nav-link @endif">
                     <i class="fas fa-tachometer-alt mr-3"></i>
                     Dashboard
                 </a>
-                <a href="{{ route('users') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                <a href="{{ route('users') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item @if($path == 'users') active-nav-link @endif">
                     <i class="fas fa-users mr-3"></i>
                     Usuários
                 </a>
@@ -72,45 +79,14 @@
 
                 <!-- Dropdown Nav -->
                 <nav :class="isOpen ? 'flex': 'hidden'" class="flex flex-col pt-4">
-                    <a href="index.html" class="flex items-center active-nav-link text-white py-2 pl-4 nav-item">
+                    <a href="{{ route('home') }}" class="flex items-center text-white py-2 pl-4 nav-item @if($path == '') active-nav-link @endif">
                         <i class="fas fa-tachometer-alt mr-3"></i>
                         Dashboard
                     </a>
-                    <a href="blank.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-sticky-note mr-3"></i>
-                        Blank Page
+                    <a href="{{ route('users') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item" @if($path == 'users') active-nav-link @endif>
+                        <i class="fas fa-users mr-3"></i>
+                        Usuários
                     </a>
-                    <a href="tables.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-table mr-3"></i>
-                        Tables
-                    </a>
-                    <a href="forms.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-align-left mr-3"></i>
-                        Forms
-                    </a>
-                    <a href="tabs.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-tablet-alt mr-3"></i>
-                        Tabbed Content
-                    </a>
-                    <a href="calendar.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-calendar mr-3"></i>
-                        Calendar
-                    </a>
-                    <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-cogs mr-3"></i>
-                        Support
-                    </a>
-                    <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-user mr-3"></i>
-                        My Account
-                    </a>
-                    <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-                        <i class="fas fa-sign-out-alt mr-3"></i>
-                        Sign Out
-                    </a>
-                    <button class="w-full bg-white cta-btn font-semibold py-2 mt-3 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
-                        <i class="fas fa-arrow-circle-up mr-3"></i> Upgrade to Pro!
-                    </button>
                 </nav>
             </header>
 
